905067da915de57f50d00f364ec94ef192492b47
Marie-Ehrman <marie.e.ehrman@gmail.com>
2019-09-08 14:18:46 -0700

    Unit 3 Project
    Interactive Form

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
 /*********** Marie Ehrman's Unit 3 Project. Interactive Form ***********/
 
 
+
+//global variables for inputs
+
+const $name = $('#name').val();
+const $email = $('#email').val();
+const $ccNum = $('#cc-num').val();
+const $ccZip = $('#zip').val();
+const $cvv = $('#cvv').val();
+
+
 //focus the name field when the page loads
 $('#name').focus();
+
+
+/********** JOB ROLE SECTION **********/
+
 
 //initially the 'other job' description in job role section
 $('#other_title').hide();
 
+
+//show the "Other" field if other is chosen under Job Role
+$('#title').change( function () {
+
+    if( $(this).val() === 'other'){
+
+         $('#other_title').show();
+
+    } else {
+
+        $('#other_title').hide();
+    }
+});
 
 //hide the “Select Theme” <option> element from showing up in the in the dropdwon menu.
 $('#design option').eq(0).hide();
@@ -29,7 +56,8 @@ $('#color option').each(function (i) {
 $('#design').change(function (){
 
     $('#color option').each(function (i) {
-            $('#color option').eq(0).hide();
+
+        $('#color option').eq(0).hide();
         
         //if "JS Puns" is chosen as the shirt design, enter into this conditional
         if ( $('#design').val() === 'js puns' ){
@@ -85,9 +113,13 @@ let $totalCost = 0;
 //Listen for changes in the Activity section
 $('.activities').change( function (e) {
 
+
+
     //variable to see if the boxes are checked
     const $checkbox = e.target;
+
  
+
     //variable to collect the 'data-cost' from the checked box
     let $activityCost = $($checkbox).attr('data-cost');
 
@@ -104,7 +136,6 @@ $('.activities').change( function (e) {
     //set the text of the total cost element equal to the string ‘Total:
     //$’ with the current value of the total cost variable
     $($totalCostElement).text('Total: $' + $totalCost);
-
 
 
     //DISABLE CONFLICTING ACTIVITIES
@@ -138,7 +169,9 @@ $('.activities').change( function (e) {
 
                 }
         }
+
     });
+
 });
 
 
@@ -177,3 +210,119 @@ $('#payment').change(function (){
 
 /********** FORM VALIDATION SECTION **********/
 
+function nameValidation ($name){
+
+    if (/^[A-Za-z]+(\s[A-Za-z]+)?(\s[A-Za-z]+)?$/.test($name) === false){
+
+        $('#name').append('<span>Please enter your name</span>');
+        $('#name').css('border', 'red');
+        return /^[A-Za-z]+(\s[A-Za-z]+)?(\s[A-Za-z]+)?$/.test($('#name').val());
+
+    } else {
+
+        $('#name').css('border', 'green');
+        return /^[A-Za-z]+(\s[A-Za-z]+)?(\s[A-Za-z]+)?$/.test($('#name').val());
+
+    }
+}
+
+function emailValidation ($email){
+
+    if( /^[^@]+@[^@.]+\.[a-z]+(\.[a-z]+)?$/i.test($email) === false){
+
+        $('#email').append('<span>Please enter your email address</span>');
+        $('#email').css('border', 'red');
+        return /^[^@]+@[^@.]+\.[a-z]+(\.[a-z]+)?$/i.test($email);
+
+    } else {
+
+        $('#email').css('border', 'green');
+        return /^[^@]+@[^@.]+\.[a-z]+(\.[a-z]+)?$/i.test($email);
+
+    }
+}
+
+//Activity 
+function activityValidation ($activityInput){
+
+    $($activityInput).each( function (i) {
+
+        if ( $($checkbox).eq(i).prop('checked') === false){
+
+            $('.activities input').css('border', 'red');
+            return false;
+        }
+    });
+}
+
+//$('button').on('click', activityValidation ($activityInput));
+
+//Credit Card validation (only if Credit Card is picked)
+//ensure 13-16 #s are entered
+function ccNumValidation ($ccNum){
+    
+    if (/^(\d{4}\s?){3}\d{4}$/.test($ccNum) === false){
+
+        $('#cc-num').append('<span>Please enter a valid Credit Card Number</span>');
+        $('#cc-num').css('border', 'red');
+        return /^(\d{4}\s?){3}\d{4}$/.test($ccNum);
+
+    } else {
+
+        $('#cc-num').css('border', 'green');
+        return /^(\d{4}\s?){3}\d{4}$/.test($ccNum);
+
+    }
+}
+
+//Credit Card Zip validation (only if Credit Card is picked)
+function ccZipValidation ($ccZip){
+   
+    if (/^\d{5}$/.test($ccZip) === false){
+        
+        $('#zip').append('<span>Please enter a Zip Code</span>');
+        $('#zip').css('border', 'red');
+        return /^\d{5}$/.test($ccZip);
+
+    } else {
+
+        $('#zip').css('border', 'green');
+        return /^\d{5}$/.test($ccZip);
+
+    }
+}
+
+//Credit Card CVV# validation (only if Credit Card is picked)
+function cvvValidation ($cvv){
+    
+    if (/^\d{3}&/.test($cvv) === false){
+
+        $('#cvv').append('<span>Please enter a cvv(found on back of card)</span>');
+        $('#cvv').css('border', 'red');
+        return /^\d{3}&/.test($cvv);
+
+    } else {
+
+        $('#cvv').css('border', 'green');
+        return /^\d{3}&/.test($cvv);
+
+    }
+}
+
+function isValid(){
+
+    if(!nameValidation($name) || !emailValidation($email)|| !activityValidation($activityInput)){
+
+        return false;
+    }
+
+
+}
+
+
+$('form').on('submit', function(isValid){
+
+        if(isValid){
+        $('form').preventDefault();
+        }
+});
